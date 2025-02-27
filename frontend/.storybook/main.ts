9a8c7a1a60c77977f78adb7f694b18bc3abe625d
Danyil <danilagarma1999@mail.ru>
2025-02-27 11:08:26 +0200

    feat(storybook): initialize Storybook with Next.js support

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+    stories: [
+        "../src/**/*.mdx",
+        "../src/shared/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../src/widgets/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../src/entities/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../src/features/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-essentials",
+        "@storybook/addon-onboarding",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions",
+    ],
+    framework: {
+        name: "@storybook/nextjs",
+        options: {},
+    },
+};
+
+export default config;
