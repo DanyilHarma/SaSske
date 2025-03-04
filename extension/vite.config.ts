8d2bf3bdbbdf5af9ea1b6b571654ae3cc8f61f30
Danyil <danilagarma1999@mail.ru>
2025-03-04 17:31:31 +0200

    feat(extension): add manifest.json, background, content & popup setup

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "manifest.json",
+                    dest: ".",
+                },
+            ],
+        }),
+    ],
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: {
+                popup: "src/popup/popup.html",
+                background: "src/background/background.ts",
+                content: "src/content/content.ts",
+            },
+            output: {
+                entryFileNames: "scripts/[name].js",
+                chunkFileNames: "scripts/[name].js",
+                assetFileNames: "assets/[name].[ext]",
+            },
+        },
+    },
+});
