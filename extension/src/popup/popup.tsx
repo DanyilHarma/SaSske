dc5e543c5685e855348a94cb5f88a82bc06a61b2
Danyil <danilagarma1999@mail.ru>
2025-03-04 17:31:31 +0200

    feat(extension): add manifest.json, background, content & popup setup

---
+++ extension/src/popup/popup.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+
+const Popup = () => {
+    return <h1>Hello from Popup!</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Popup />);
