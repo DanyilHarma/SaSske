c078d767b3530e7691d4952036673aaf2d9ccbc8
Danyil <danilagarma1999@mail.ru>
2025-03-05 16:16:14 +0200

    feat(extension): add icons, fix paths in manifest.json, enable extension loading

--- extension/src/popup/popup.tsx
+++ extension/src/popup/popup.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import "../index.css";
 
 const Popup = () => {
-    return <h1>Hello from Popup!</h1>;
+    return <h1 className="text-red-400">Hello from Popup!</h1>;
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<Popup />);
