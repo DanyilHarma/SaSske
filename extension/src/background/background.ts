0620ba4ed81375dcd8e59bea1b7e74c427ad6d88
Danyil <danilagarma1999@mail.ru>
2025-03-07 14:48:33 +0200

    refactor(extension):optimize event handling with throttling

--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -1,12 +1,33 @@
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === "SAVE_TIME") {
-        chrome.storage.local.get([message.site], (result) => {
-            const previousTime = result[message.site] || 0;
-            const newTime = previousTime + message.timeSpent;
-
-            chrome.storage.local.set({ [message.site]: newTime }, () => {
-                console.log(`✅ Время на ${message.site}: ${newTime} секунд`);
-            });
+chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+    if (message.type === "GET_ACTIVE_SITE") {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            if (tabs[0]?.url) {
+                const domain = new URL(tabs[0]?.url).hostname;
+                sendResponse({ site: domain });
+            }
         });
+        return true;
+    }
+    if (message.type === "SAVE_TIME") {
+        saveTime(message.site, message.timeSpent);
     }
 });
+
+// Функция сохранения времени в локальное хранилище
+function saveTime(site: string, timeSpent: number) {
+    chrome.storage.local.get([site], (result) => {
+        const previousTime = result[site] || 0;
+        const newTime = previousTime + timeSpent;
+
+        chrome.storage.local.set({ [site]: newTime }, () => {
+            if (chrome.runtime.lastError) {
+                console.error(
+                    "Ошибка при сохранении в `chrome.storage.local`:",
+                    chrome.runtime.lastError,
+                );
+            } else {
+                console.log(`✅ Время на ${site}: ${newTime} секунд`);
+            }
+        });
+    });
+}
