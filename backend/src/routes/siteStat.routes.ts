508d1c8767fbe830af035f1194b635a946c36124
Danyil <danilagarma1999@mail.ru>
2025-04-08 17:51:32 +0300

    feat(backend-swagger): add Swagger doc for POST /site-stat endpoint

--- backend/src/routes/siteStat.routes.ts
+++ backend/src/routes/siteStat.routes.ts
@@ -3,6 +3,35 @@ import { createSiteStat } from "../controllers/siteStat.controllers";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /site-stat:
+ *   post:
+ *     summary: Save time spent on a website
+ *     tags: [SiteStat]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               site:
+ *                 type: string
+ *               timeSpent:
+ *                 type: integer
+ *             required:
+ *               - site
+ *               - timeSpent
+ *     responses:
+ *       201:
+ *         description: Time entry created
+ *       400:
+ *         description: Invalid input
+ *       500:
+ *         description: Server error
+ */
+
 router.post("/site-stat", createSiteStat);
 console.log(createSiteStat);
 export default router;
