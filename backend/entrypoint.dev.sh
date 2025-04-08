478af298ca77eac2066388e9a1c8fe7c50e5712a
Danyil <danilagarma1999@mail.ru>
2025-04-08 17:10:51 +0300

    chore(docker): add node_modules auto-install to backend entrypoint script

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Устанавливаем зависимости, если node_modules нет
+if [ ! -d "node_modules" ]; then
+  echo "📦 Installing node modules..."
+  npm install
+fi
+
 # Применение миграций (или можно на проде не делать, если управляется вручную)
 echo "🔄 Running migrations..."
 npx prisma migrate deploy
